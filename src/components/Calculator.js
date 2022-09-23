8369ed284cc0b8e166fb056cf1d2934026734d66
Saikat Chakraborty <saikatgpae@gmail.com>
2022-09-23 15:22:16 +0530

    Add calculator function.

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,70 @@
 import React from 'react';
-import CalculatorButtons from './calculatorButtons';
+import CalculatorButtons from './CalculatorButtons';
 import './style.css';
+import calculate from '../logic/calculate';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.changeObject = this.changeObject.bind(this);
+    this.state = {
+      obj: {
+        total: null,
+        next: null,
+        operation: null,
+      },
+      display: '',
+    };
+  }
+
+  changeObject(buttn) {
+    const { obj } = this.state;
+    const newObj = calculate(obj, buttn);
+    this.setState(() => ({
+      obj: newObj,
+    }));
+    if (buttn === '=') {
+      this.setState(() => ({
+        display: newObj.total,
+      }));
+    } else if (buttn === 'AC') {
+      this.setState(() => ({
+        display: '',
+      }));
+    } else {
+      this.setState((state) => ({
+        display: state.display + buttn,
+      }));
+    }
+  }
+
   render() {
+    const { display } = this.state;
     return (
       <div className="calculator">
-        <div className="display p-3"><h2 id="display">0</h2></div>
+        <div className="display p-3"><h2 id="display">{display}</h2></div>
         <div className="all-button">
-          <CalculatorButtons label="AC" clName="white-background" />
-          <CalculatorButtons label="+/-" clName="white-background" />
-          <CalculatorButtons label="%" clName="white-background" />
-          <CalculatorButtons label="+" clName="orange-background" />
-          <CalculatorButtons label="7" clName="white-background" />
-          <CalculatorButtons label="8" clName="white-background" />
-          <CalculatorButtons label="9" clName="white-background" />
-          <CalculatorButtons label="X" clName="orange-background" />
-          <CalculatorButtons label="4" clName="white-background" />
-          <CalculatorButtons label="5" clName="white-background" />
-          <CalculatorButtons label="6" clName="white-background" />
-          <CalculatorButtons label="-" clName="orange-background" />
-          <CalculatorButtons label="1" clName="white-background" />
-          <CalculatorButtons label="2" clName="white-background" />
-          <CalculatorButtons label="3" clName="white-background" />
-          <CalculatorButtons label="+" clName="orange-background" />
-          <CalculatorButtons label="0" clName="white-background" />
-          <CalculatorButtons label="0" clName="white-background" />
-          <CalculatorButtons label="." clName="white-background" />
-          <CalculatorButtons label="=" clName="orange-background" />
+          <CalculatorButtons label="AC" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="+/-" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="%" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="+" clName="orange-background" onClick={this.changeObject} />
+          <CalculatorButtons label="7" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="8" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="9" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="x" clName="orange-background" onClick={this.changeObject} />
+          <CalculatorButtons label="4" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="5" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="6" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="-" clName="orange-background" onClick={this.changeObject} />
+          <CalculatorButtons label="1" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="2" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="3" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="+" clName="orange-background" onClick={this.changeObject} />
+          <CalculatorButtons label="0" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="0" clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="." clName="white-background" onClick={this.changeObject} />
+          <CalculatorButtons label="=" clName="orange-background" onClick={this.changeObject} />
         </div>
       </div>
     );
--- src/components/calculatorButtons.js
+++ src/components/calculatorButtons.js
@@ -3,15 +3,26 @@ import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class CalculatorButtons extends React.Component {
+  constructor(props) {
+    super(props);
+    this.bhandleObjectChange = this.bhandleObjectChange.bind(this);
+  }
+
+  bhandleObjectChange() {
+    const { label, onClick } = this.props;
+    onClick(label);
+  }
+
   render() {
     const { label, clName } = this.props;
-    return <button className={`${clName}`} type="submit">{label}</button>;
+    return <button className={clName} type="submit" onClick={this.bhandleObjectChange}>{label}</button>;
   }
 }
 
 CalculatorButtons.propTypes = {
   label: PropTypes.string.isRequired,
   clName: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
 
 export default CalculatorButtons;
